ddb644ab8487b973fc5468d1249d6cd2d435871b
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-29 10:59:00 +0530

    use props to export item component object to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
 import {ButtonComponent} from "./assets/component/ButtonComponent.tsx";
+import {Item} from "./assets/component/Item.tsx";
 
 function App() {
+    const itemList =[
+        {
+            title : "title1",
+            description: "description1",
+        },
+        {
+            title : "title2",
+            description: "description2",
+        },
 
+        {
+            title : "title3",
+            description: "description3",
+        }
+    ]
   return (
       <>
           <ButtonComponent name = 'add'/>
+
+          <Item title={itemList[0].title} description={itemList[0].description} />
+          <Item title={itemList[1].title} description={itemList[1].description} />
+          <Item title={itemList[2].title} description={itemList[2].description} />
       </>
   )
 }
---
+++ src/assets/component/Item.tsx
@@ -0,0 +1,8 @@
+export function Item(props: {title: string, description: string}) {
+    return (
+        <>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </>
+    );
+}
