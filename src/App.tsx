be41a1cde1db46311085cde5a16de9ed81f0baab
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-29 16:29:38 +0530

    make number incrementer with use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,15 @@
 import './App.css';
-import ButtonComponent from "./assets/component/ButtonComponent";
 import { useState } from "react";
+import ButtonComponent from "./assets/component/ButtonComponent.tsx";
 
 function App() {
-    // State to manage content
-    const [content, setContent] = useState("default");
-
-    // Subjects array
-    const subjects = [
-        {
-            sName: "RAD",
-            sDes: "Lorem Ipsum RAD"
-        },
-        {
-            sName: "MAD",
-            sDes: "Lorem Ipsum MAD"
-        },
-        {
-            sName: "OOP",
-            sDes: "Lorem Ipsum OOP"
-        },
-        {
-            sName: "AAD",
-            sDes: "Lorem Ipsum AAD"
-        }
-    ];
-
-    // Click handler
-    const handleClick = (name: string) => {
-        const subject = subjects.find((subject) => subject.sName === name);
-
-        if (subject) {
-            setContent(subject.sDes); // Update content with description
-            alert(subject.sDes); // Show description in alert
-        }
-    };
-
+    const [count, setCount] = useState(0)
     return (
-        <div>
-            {subjects.map((subject) => (
-                <ButtonComponent
-                    key={subject.sName}
-                    onSelect={() => handleClick(subject.sName)}
-                >
-                    {subject.sName}
-                </ButtonComponent>
-            ))}
-            <div>
-                <h2>Selected Content:</h2>
-                <p>{content}</p>
-            </div>
-        </div>
+        <>
+            {count}<br/>
+            <ButtonComponent onSelect={() => setCount(count + 1)}>addition</ButtonComponent>
+            <ButtonComponent onSelect={() => setCount(count + 1)}>subtraction</ButtonComponent>
+        </>
     );
 }
 
