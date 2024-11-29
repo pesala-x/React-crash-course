073066502f7b1f02b1d506ebca0f43d291229f31
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-29 09:58:31 +0530

    make ButtonComponent and use it in App.tsx

---
+++ src/assets/component/ButtonComponent.tsx
@@ -0,0 +1,9 @@
+export function ButtonComponent() {
+    return (
+        <>
+            <button>click mee...</button>
+            <button>click mee...</button>
+            <button>click mee...</button>
+        </>
+    );
+}
