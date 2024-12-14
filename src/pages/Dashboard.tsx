af22335be51c6315acb07e69eb1a1c40865a3dab
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-14 15:15:42 +0530

    Dashboard page styles done with add output like a table

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,37 @@
-import {CustomerContext} from "../components/CustomerProvider.tsx";
-import {useContext} from "react";
-import {Customer} from "../models/customer.ts";
+import { CustomerContext } from "../components/CustomerProvider.tsx";
+import { useContext } from "react";
+import { Customer } from "../models/customer.ts";
 
 export function Dashboard() {
-
     // @ts-ignore
-    const [customers, setCustomers] = useContext(CustomerContext);
+    const [customers] = useContext(CustomerContext);
+
     return (
         <>
-            <header className="mt-8 mb-6">
+            <header className="mt-8 mb-6 text-center">
                 <h1 className="text-6xl font-bold text-gray-100">Dashboard</h1>
             </header>
-            {customers.map((customer: Customer) => (
-                <div key={customer.email}>{customer.name + ' ' + customer.email + ' ' + customer.phone}</div>))}
+
+            <div className="overflow-x-auto shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md">
+                <table className="min-w-full table-auto border-collapse border border-gray-700 text-white">
+                    <thead>
+                        <tr className="bg-[#03001417] backdrop-blur-md">
+                            <th className="border border-gray-700 px-4 py-2 text-center">Name</th>
+                            <th className="border border-gray-700 px-4 py-2 text-center">Email</th>
+                            <th className="border border-gray-700 px-4 py-2 text-center">Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {customers.map((customer: Customer, index: number) => (
+                        <tr key={index} className="odd:bg-gray-700 even:bg-gray-600">
+                            <td className="border border-gray-700 px-4 py-2">{customer.name}</td>
+                            <td className="border border-gray-700 px-4 py-2">{customer.email}</td>
+                            <td className="border border-gray-700 px-4 py-2">{customer.phone}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
